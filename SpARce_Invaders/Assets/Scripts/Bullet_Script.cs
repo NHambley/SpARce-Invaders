5e684adf05065bd36568710ce64d6727efa3337e
NHambley <nhambley6@gmail.com>
2019-03-28 14:24:43 -0400

    added bullet shooting

---
+++ SpARce_Invaders/Assets/Scripts/Bullet_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Script : MonoBehaviour
+{
+    GameObject player;
+    Vector3 movementVec;
+    Vector3 position;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        position = transform.position;
+        movementVec = player.transform.forward;
+        speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position += movementVec * speed * Time.deltaTime;
+    }
+}
---
+++ SpARce_Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bullet; 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check for touch input
+        if(Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+
+    private void FireBullet()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
