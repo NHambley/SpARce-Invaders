b3da42b7162801093f4fb7807f55e16ffb2c7270
James Schrupp <jds5583@rit.edu>
2019-03-28 16:49:25 -0400

    Enemy01
    Enemy moves towards player, respawns if dead (no bullet detection yet).

---
+++ SpARce_Invaders/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 position;
+    private Vector3 destination;
+    public bool alive;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alive)
+        {
+            position = transform.position;
+            destination = player.transform.position;
+            Move();
+        }
+        else
+        {
+            Vector2 randomXZ = Random.insideUnitCircle * 10;
+            Vector3 newPosition = new Vector3(randomXZ.x, 5, randomXZ.y);
+            GameObject newEnemy = Instantiate(gameObject, newPosition, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void Move()
+    {
+        Vector3 direction = (destination - position).normalized;
+        
+        Vector3 scale = new Vector3(0.01f, 0.01f, 0.01f);
+        direction.Scale(scale);
+        position += direction;
+        transform.position = position;
+    }
+}
